d624530b7a3531328786847a4158929f2e3b9459
GuilhermeGarcia <guilherme.garcia1136@gmail.com>
2024-02-29 20:49:32 -0300

    fix redis export error

--- src/lib/redis.ts
+++ src/lib/redis.ts
@@ -1,4 +1,4 @@
 import { Redis } from "ioredis";
 
 
-const redis = process.env.ENVIRONMENT === "production" ? new Redis() : new Redis(process.env.KV_URL as string);
+export const redis = process.env.ENVIRONMENT === "production" ? new Redis() : new Redis(process.env.KV_URL as string);
